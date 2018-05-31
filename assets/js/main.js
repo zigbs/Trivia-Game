016d209dab41de1303ecc1571c8e1cb18ffe4e90
Tyler Brown <zigbs.zigbs@gmail.com>
2018-05-30 23:14:34 -0700

    random event now dynamically linked to length of question array

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     //variable declarations go here
-    
+
 
     //object declarations go here
     var gameObject = {
@@ -14,54 +14,51 @@ $(document).ready(function () {
         totalHints: 0,  //This is the tally for the # of hints that they use throughtout the game
         totalTries: 0,  //This is the tally for the # of total tries that the user uses in a round of play
         menu: true,
-        hasClicked : false,
+        hasClicked: false,
         difficulty: 2, //by default this is hard.  There are no tries nor are there hints.  This was done to get a minimally viable program
-        questionsObject : [
-            { question: "Who made first contact with Earth?",
-              answers: ["Vulcans", "Romulans", "Klingons", "The Breen"],
-              correct: "Vulcans",
-              hints: ["Logic defines them", "Live long and prosper"],  },
-            
-            { question: "Where do the Maqui live?",
-              answers: ["Bajor", "The Badlands", "Cardassia", "Earth"],
-              correct: "The Badlands",
-              hints: ["A desert wasteland", "Land that nobody wanted"],  },
-            
-              { question: "What color is Vulcan blood?",
-              answers: ["Green", "Red", "Magenta", "Sky Blue"],
-              correct: "Green",
-              hints: ["Not normal", "Reminds you of a bug"],  },
+        questionsObject: [
+            {
+                question: "Who made first contact with Earth?",
+                answers: ["Vulcans", "Romulans", "Klingons", "The Breen"],
+                correct: "Vulcans",
+                hints: ["Logic defines them", "Live long and prosper"],
+            },
 
-              { question: "What do the Borg always say?",
-              answers: ["Resistance is Futile", "Live Long and Prosper", "To Romulus!", "Cooperation is Key"],
-              correct: "Resistance is Futile",
-              hints: ["A subtle message", "You should not try it"],  },
+            {
+                question: "Where do the Maqui live?",
+                answers: ["Bajor", "The Badlands", "Cardassia", "Earth"],
+                correct: "The Badlands",
+                hints: ["A desert wasteland", "Land that nobody wanted"],
+            },
 
-            { question: "Which race is NOT a Vulcanoid?",
-              answers: ["Garidian", "Vulcan", "Romulan", "Vorta"],
-              correct: "Vorta",
-              hints: ["Not from the Alpha or Beta Quadrants", "Affiliated with Dominion command positions"],  }
-            ],
+            {
+                question: "What color is Vulcan blood?",
+                answers: ["Green", "Red", "Magenta", "Sky Blue"],
+                correct: "Green",
+                hints: ["Not normal", "Reminds you of a bug"],
+            },
+
+            {
+                question: "What do the Borg always say?",
+                answers: ["Resistance is Futile", "Live Long and Prosper", "To Romulus!", "Cooperation is Key"],
+                correct: "Resistance is Futile",
+                hints: ["A subtle message", "You should not try it"],
+            },
+
+            {
+                question: "Which race is NOT a Vulcanoid?",
+                answers: ["Garidian", "Vulcan", "Romulan", "Vorta"],
+                correct: "Vorta",
+                hints: ["Not from the Alpha or Beta Quadrants", "Affiliated with Dominion command positions"],
+            }
+        ],
         resetVariables: function () {
             this.totalWins = 0;
             this.totalLosses = 0;
+            this.triesLeft = 0;
         },
         mainMenuLoad: function () {
-            //$('#game-container').empty();
             this.resetVariables();
-        },
-        receiveQuestion: function () {
-            $('#main-menu').hide();
-            $('#result').hide();
-            $('#receive-question').show();
-            this.loadHintTry();
-            this.outputInfo();
-        },
-        giveAnswer: function () {
-            $('#main-menu').hide();
-            $('#result').show();
-            $('#receive-question').hide();
-            
         },
         loadGame: function () {
             $('#main-menu').show();
@@ -70,19 +67,16 @@ $(document).ready(function () {
             this.mainMenuLoad();
         },
         loadHintTry: function () {
-        // THIS LOADS THE HINT AND TRY BUTTONS.  YOU CAN ASSIGN VALUES LATER
-            
-            if(this.difficulty === 2) {
-                
+            if (this.difficulty === 2) {
                 $('#hint-button-1').empty();
                 $('#hint-button-2').empty();
-            } 
-            if(this.difficulty === 1) {
+            }
+            if (this.difficulty === 1) {
                 var htmlString = '<button type="button" class="btn btn-warning" id="hint-button-1">Hint 1</button>';
                 $('#hint-button-1').empty().append(htmlString);
                 $('#hint-button-2').empty();
-            } 
-            if(this.difficulty === 0) {
+            }
+            if (this.difficulty === 0) {
                 var htmlString = '<button type="button" class="btn btn-warning" id="hint-button-1">Hint 1</button>';
                 var htmlString2 = '<button type="button" class="btn btn-warning" id="hint-button-2">Hint 2</button>';
                 $('#hint-button-1').empty().append(htmlString);
@@ -90,15 +84,24 @@ $(document).ready(function () {
             }
         },
         outputInfo: function () {
-            var randomize = Math.floor(Math.random() * 5); 
-            console.log(randomize);
+            var questionArray = gameObject.questionsObject.length;
+            console.log(questionArray + ' Is the length of question Array ');
+            var randomize = Math.floor(Math.random() * questionArray);
+            console.log(randomize + " is the random value selected");
             console.log(this.questionsObject[randomize].hints[0]);
 
             var htmlString = '<h1>' + this.questionsObject[randomize].question + '</h1>';
-            
+
+            var hintsRemString = gameObject.hintsLeft;
+            $('#hints-remaining').empty().append('There are ' + hintsRemString +' hints left.');
+
             //HERE IS WHERE THE QUESTIONS SPIT OUT
             $('#question').empty().append(htmlString);
-                     
+
+            $('#menu-button').click(function () {
+                gameObject.loadGame();
+            });
+
             var AnswerAString = '<button type="button" class="btn btnans btn-lg btn-success" value="' + this.questionsObject[randomize].answers[0] + '">' + this.questionsObject[randomize].answers[0] + '</button>';
             $('#answer-a').empty().append(AnswerAString);
             var AnswerBString = '<button type="button" class="btn btnans btn-lg btn-success value="' + this.questionsObject[randomize].answers[1] + '">' + this.questionsObject[randomize].answers[1] + '</button>';
@@ -118,71 +121,87 @@ $(document).ready(function () {
             $('#second-hint').hide().append(Hint2String);
             console.log(Hint2String);
 
-            $('#hint-button-1').click(function(){
+            $('#hint-button-1').click(function () {
+                if (gameObject.hintsLeft > 0){
                 $('#first-hint').show();
+                    gameObject.updateHints();
+                }
             });
-            $('#hint-button-2').click(function(){
+
+            $('#hint-button-2').click(function () {
+                if (gameObject.hintsLeft > 0){
                 $('#second-hint').show();
+                    gameObject.updateHints();
+            }
             });
-
-            
-            $('.btnans').click(function (){
-                var answerValue = $('.btnans').val();
-                console.log(answerValue);
-            });
-
         },
         checkMenuClicks: function () {
-            if(gameObject.hasClicked === false){
-                $('#easy-select').click(function (){
-    
-                    gameObject.difficulty = 0;
-                    console.log(gameObject.difficulty);
-                });
-                $('#medium-select').click(function (){
-    
-                    gameObject.difficulty = 1;
-                    console.log(gameObject.difficulty);
-                });
-                $('#hard-select').click(function (){
-                    
-                    gameObject.difficulty = 2;
-                    console.log(gameObject.difficulty);
-                });
-                $('.diff-select').click(function (){
-                    gameObject.hasClicked = true;
-                    setTimeout(function () {
-                        gameObject.receiveQuestion();
-                    }, 2000);
-                
-                });
-            }
+
+            $('#easy-select').click(function () {
+
+                gameObject.difficulty = 0;
+                console.log(gameObject.difficulty);
+                gameObject.hintsLeft = 2;
+            });
+            $('#medium-select').click(function () {
+
+                gameObject.difficulty = 1;
+                console.log(gameObject.difficulty);
+                gameObject.hintsLeft = 1;
+            });
+            $('#hard-select').click(function () {
+
+                gameObject.difficulty = 2;
+                console.log(gameObject.difficulty);
+                gameObject.hintsLeft = 0;
+            });
+            $('.diff-select').click(function () {
+                gameObject.hasClicked = true;
+                setTimeout(function () {
+                    gameObject.receiveQuestion();
+                }, 2000);
+
+            });
+        },
+        receiveQuestion: function () {
+            $('#main-menu').hide();
+            $('#result').hide();
+            $('#receive-question').show();
+            this.loadHintTry();
+            this.outputInfo();
+        },
+        updateHints: function () {
+            gameObject.hintsLeft -= 1;
+            console.log(gameObject.hintsLeft);
+            gameObject.totalHints++;
+            console.log("You user have used a total of" + gameObject.totalHints + "hints");
+            var hintsRemString1 = gameObject.hintsLeft;
+            $('#hints-remaining').empty().append('There are ' + hintsRemString1 +' hints left.');
+        },
+        giveAnswer: function () {
+            $('#main-menu').hide();
+            $('#result').show();
+            $('#receive-question').hide();
+
+            $('#answer-keep-going').click(function () {
+                gameObject.receiveQuestion();
+            });
+        
+            $('#answer-menu-backout').click(function () {
+                gameObject.loadGame();
+            });
         }
+
     };
-        //BY DEFAULT THE MENU IS DISPLAYED
-        gameObject.loadGame();
-        gameObject.checkMenuClicks();
-                
+    //BY DEFAULT THE MENU IS DISPLAYED
+    gameObject.loadGame();
+    gameObject.checkMenuClicks();
 
 
 
-        //The quesiton screen navigation & events
-
-        
 
 
-        $('#menu-button').click(function (){
-            gameObject.loadGame();
-        });
+    
 
-        //answer navigation
-        $('#answer-keep-going').click(function(){
-            gameObject.receiveQuestion();
-        });
 
-        $('#answer-menu-backout').click(function(){
-            gameObject.loadGame();
-        });
-
-        
 });
